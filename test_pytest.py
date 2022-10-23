7c512eda13603d681e09d30672989596d1cf4a49
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-23 11:47:21 +0300

    I Did the task1

--- test_pytest.py
+++ test_pytest.py
@@ -1,18 +1,30 @@
-"""
-Задание: Следует протестировать основные функции по получению информации о документах,
-добавлении и удалении элементов из словаря.
-"""
-
 import pytest
+from app import check_document_existance, get_doc_owner_name, delete_doc, get_doc_shelf
+
+# Task 1
+def test_check_document_existance():
+    result = check_document_existance("10006")
+    assert True == result
+
+@pytest.mark.parametrize('number, result', [
+    ("2207 876234", "Василий Гупкин"),
+    ("11-2", "Геннадий Покемонов"),
+    ("10006", "Аристарх Павлов")
+
+])
+def test_get_doc_owner_name(number, result):
+    res = get_doc_owner_name(number)
+    assert result == res
 
 
+def test_delete_doc():
+    res = delete_doc(["2207 876234"])
+    assert res != True
 
 
+@pytest.mark.parametrize('user_doc_number, result', [('10006', '2'), ('2207 876234', '1')])
+def test_get_doc_shelf(user_doc_number, result):
+    res =get_doc_shelf(user_doc_number)
+    assert res == result
 
 
-
-
-
-
-if __name__ == '__main__':
-    pass
